5436b9dadaac8af03decd3c61ae138ded2dd2844
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2020-06-09 13:01:41 +0300

    Migrator corner cases and main func (#175)

--- packages/operator/pkg/database/migrators/postgres/migrator.go
+++ packages/operator/pkg/database/migrators/postgres/migrator.go
@@ -1,11 +1,13 @@
 package postgres
 
 import (
-	"fmt"
 	"github.com/golang-migrate/migrate"
 	_ "github.com/golang-migrate/migrate/database/postgres"
 	_ "github.com/golang-migrate/migrate/source/file"
+	"github.com/prometheus/common/log"
 )
+
+const migrationsSource = "file:///Users/vladislav_tokarev/go/src/github.com/odahu/odahu-flow/packages/operator/pkg/database/migrations/postgres"
 
 type Migrator struct {
 	ConnString string
@@ -13,16 +15,37 @@ type Migrator struct {
 
 func (m Migrator) MigrateToLatest() error {
 
-	// TODO: corner cases
-
-	fmt.Print("Migrate to latest")
-	migrateSource := "file:///Users/vladislav_tokarev/go/src/github.com/odahu/odahu-flow/packages/operator/pkg/database/migrations/postgres"
-	mClient, err := migrate.New(migrateSource, m.ConnString)
+	mClient, err := migrate.New(migrationsSource, m.ConnString)
 	if err != nil {
 		return err
 	}
 
+	version, _, _ := mClient.Version()
 	err = mClient.Up()
+	newVersion, _, _ := mClient.Version()
+	if err != nil {
+		switch {
+		case err == migrate.ErrNoChange || err == migrate.ErrNilVersion:
+			log.Infof(
+				"No migrations was applied. Old version: %d, new version: %d",
+				version, newVersion,
+			)
+		default:
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m Migrator) DownAllMigrations() error {
+
+	mClient, err := migrate.New(migrationsSource, m.ConnString)
+	if err != nil {
+		return err
+	}
+
+	err = mClient.Down()
 	if err != nil {
 		return err
 	}
