0e6f5363483142237a527457f3b28638c7e88546
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2020-06-12 19:29:47 +0300

    Fix DI for Webserver in case when postgres backend for Toolchain Integration (#175)

--- packages/operator/pkg/webserver/routes/v1/training/routes.go
+++ packages/operator/pkg/webserver/routes/v1/training/routes.go
@@ -23,17 +23,11 @@ import (
 	mt_repository "github.com/odahu/odahu-flow/packages/operator/pkg/repository/training"
 )
 
-func ConfigureRoutes(
-	routeGroup *gin.RouterGroup,
-	mtRepository mt_repository.Repository,
-	connRepository conn_repository.Repository,
-	config config.ModelTrainingConfig,
-	gpuResourceName string,
-) {
+func ConfigureRoutes(routeGroup *gin.RouterGroup, mtRepository mt_repository.Repository, tiRepository mt_repository.ToolchainRepository, connRepository conn_repository.Repository, config config.ModelTrainingConfig, gpuResourceName string) {
 	mtController := ModelTrainingController{
 		mtRepository: mtRepository,
 		validator: NewMtValidator(
-			mtRepository,
+			tiRepository,
 			connRepository,
 			config.DefaultResources,
 			config.OutputConnectionID,
