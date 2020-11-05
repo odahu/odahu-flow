38c299c92647c638a0441bd713c5c3e4de574e68
BPylypenko <49116723+BPylypenko@users.noreply.github.com>
2020-11-05 15:37:26 +0200

    if artifact name ends with ".zip"

--- packages/sdk/odahuflow/sdk/local/packaging.py
+++ packages/sdk/odahuflow/sdk/local/packaging.py
@@ -36,6 +36,11 @@ def start_package(packager: K8sPackager, artifact_path: str) -> Dict[str, Any]:
     if not artifact_path:
         artifact_path = config.LOCAL_MODEL_OUTPUT_DIR
 
+    # Removing .zip extension if there's one
+    artifact_name = packager.model_packaging.spec.artifact_name.strip()
+    if artifact_name.endswith('.zip'):
+        packager.model_packaging.spec.artifact_name = artifact_name[:-4]
+
     artifact_path = join(
         artifact_path,
         packager.model_packaging.spec.artifact_name
