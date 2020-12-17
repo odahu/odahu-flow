ca1bf0a99e68d210e10eb6c4663def04c962647c
BPylypenko <49116723+BPylypenko@users.noreply.github.com>
2020-12-17 15:08:17 +0200

    fixing errors with child images of image

--- packages/tests/stuff/docker-remove-image.sh
+++ packages/tests/stuff/docker-remove-image.sh
@@ -1,138 +1,114 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # This script will remove an image, but first it will remove all child images of that image.
 
-# Errors are fatal
-set -e
-
-#
 # ANSI codes for printing out different colors.
-#
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 NC="\033[0m"
 
+## parent_short_id=$1
+## parent_id=`docker inspect --format '{{.Id}}' $1`
+##
+## get_kids() {
+##     local parent_id=$1
+##     docker inspect --format='ID {{.Id}} PAR {{.Parent}}' $(docker images -a -q) | grep "PAR ${parent_id}" | sed -E "s/ID ([^ ]*) PAR ([^ ]*)/\1/g"
+## }
+##
+## print_kids() {
+##     local parent_id=$1
+##     local prefix=$2
+##     local tags=`docker inspect --format='{{.RepoTags}}' ${parent_id}`
+##     echo "${prefix}${parent_id} ${tags}"
+##
+##     local children=`get_kids "${parent_id}"`
+##
+##     for c in $children;
+##     do
+##         print_kids "$c" "$prefix  "
+##     done
+## }
+##
+## print_kids "$parent_id" ""
 
-#
 # Check to see if the specified image exists.
-# 
-# If it does, something will be returned, if not, nothing will be returned.
-#
-function does_image_exist() {
+function sanity_check() {
 	local IMAGE=$1
+	echo "# Verifying that image ${IMAGE} exists..."
 	RESULT=$(docker images -a | grep $IMAGE || true)
 	echo $RESULT
-}
-
-# Get child images for the supplied argument.  Nothing is returned if none are found
-function get_child_images() {
-	local IMAGE=$1
-	RESULT=$(docker images --filter since=${IMAGE} --quiet 2>/dev/null | sort | uniq | tr '\n' ' ')
-	echo $RESULT
-}
-
-# Run our sanity checks.
-function sanity_check() {
-
-	local IMAGE=$1
-
-	echo "# Verifying that image ${IMAGE} exists..."
-	if test ! "$(does_image_exist ${IMAGE})"
-	then
+	if test ! "$RESULT"; then
 		echo "! "
 		echo "! Sorry, image ${IMAGE} not found!"
 		echo "! "
 		return 1
 	fi
+}
 
+# Get child images for the supplied argument. Nothing is returned if none found.
+function get_child_images() {
+  local PARENT_IMAGE=$1
+  for i in $(docker images -q); do
+      docker history $i | grep -q ${PARENT_IMAGE} && echo $i
+  done | sort -u
 }
 
 # Check for and remove stopped containers.
-function remove_stopped_containers() {
+function remove_containers() {
+  local IMAGE=$1
 
-	echo "# Checking for stopped containers..."
-	local CONTAINERS=$(docker ps -a | grep Exit | awk '{print $1}' | tr '\n' ' ')
-	if test "$CONTAINERS"
-	then
-		echo "# Removing the following stopped containers: ${CONTAINERS}"
-		docker rm ${CONTAINERS}
-	fi
-
+  echo "# Checking for containers..."
+  local CONTAINERS=$(docker ps -aq ancestor=${IMAGE})
+  if test "$CONTAINERS"; then
+    echo "# Removing the following containers: ${CONTAINERS}"
+    docker rm -f ${CONTAINERS}
+  fi
 }
 
-# Remove all tags from the supplied image.
-function remove_tags() {
-
-	local IMAGE=$1
-
-	echo "# Checking image ${IMAGE} for tags that use it..."
-	local TAGS=$(docker images | grep $IMAGE | awk '{print $1}' | sort | uniq | tr '\n' ' ')
-	if test "${TAGS}"
-	then
-		echo "# Tags found to remove: ${TAGS}"
-		docker rmi ${TAGS}
-	fi
-
-}
-
-# Remove an image if it exists.
-function remove_image() {
-
-	local IMAGE=$1
-	
-	printf "#${RED} Processing image ${IMAGE}...${NC}\n"
-	remove_child_images $IMAGE
-
-	printf "# ${GREEN}Removing image ${IMAGE}...${NC}\n"
-	remove_tags $IMAGE
-
-	if test "$(does_image_exist ${IMAGE})"
-	then
-		docker rmi $IMAGE
-	fi
-
-}
-
-# Recursively remove all child images of an image.
+# Recursively removes all child images.
 function remove_child_images() {
 
-	local MAIN_IMAGE=$1
-	local IMAGE
+  local MAIN_IMAGE=$1
 
-	echo "# Checking for child images of ${MAIN_IMAGE}...."
-	local IMAGES=$(get_child_images ${MAIN_IMAGE})
+  echo "# Checking for child images of ${MAIN_IMAGE}...."
+  local IMAGES=$(get_child_images ${MAIN_IMAGE})
 
-	if test ! "$IMAGES"
-	then
-		echo "# No child images of ${MAIN_IMAGE}, bailing out!"
-		return 0
-	fi
+  if test ! "$IMAGES"; then
+    echo "# No child images of ${MAIN_IMAGE}!"
+    return 0
+  fi
 
-	printf "# ${RED}-> Found the following child images of ${MAIN_IMAGE}: ${NC}\n"
-	echo "# ${IMAGES}"
+  printf "# ${RED}-> Found the following child images of ${MAIN_IMAGE}: ${NC}\n"
+  echo "# ${IMAGES}"
 
-	for IMAGE in $IMAGES
-	do
-		remove_image $IMAGE
-	done
+  for IMAGE in $IMAGES; do
+    remove_image $IMAGE
+  done
 
-	printf "# ${GREEN}<- Done removing child images of ${MAIN_IMAGE}${NC}\n"
-
+  printf "# ${GREEN}<- Done removing child images of ${MAIN_IMAGE}${NC}\n"
 }
 
+# Remove an image.
+function remove_image() {
+  local IMAGE=$1
+  remove_containers $IMAGE
 
-if test ! "$1"
-then
-	echo "! "
-	echo "! Syntax: $0 docker_image_id"
-	echo "! "
-	exit 1
-fi
+  printf "#${RED} Processing image ${IMAGE}...${NC}\n"
+  remove_child_images $IMAGE
 
-MAIN_IMAGE=$1
+  docker rmi $IMAGE
+}
 
-sanity_check $MAIN_IMAGE
-remove_stopped_containers
-remove_image $MAIN_IMAGE
-
-printf "# ${GREEN}Done removing image ${MAIN_IMAGE} and its children!${NC}\n"
+# if test ! "$1"; then
+#   echo "! "
+#   echo "! Syntax: $0 docker_image_id"
+#   echo "! "
+#   exit 1
+# fi
+#
+# MAIN_IMAGE=$1
+#
+# sanity_check $MAIN_IMAGE
+# remove_image $MAIN_IMAGE
+#
+# printf "# ${GREEN}Done removing image ${MAIN_IMAGE} and its children!${NC}\n"
