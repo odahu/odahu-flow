2e2edd5410ce01194fd2785cedd79a94ca0edbcc
BPylypenko <49116723+BPylypenko@users.noreply.github.com>
2021-01-22 11:36:54 +0200

    fixed. returned back gcloud auth configure-docker

--- packages/tests/stuff/training_stuff.sh
+++ packages/tests/stuff/training_stuff.sh
@@ -243,6 +243,9 @@ function local_setup() {
   wget -O "${LOCAL_TEST_DATA}/conda.yaml" "${GIT_REPO_DATA}/mlflow/sklearn/wine/conda.yaml"
   wget -O "${LOCAL_TEST_DATA}/wine-quality.csv" "${GIT_REPO_DATA}/mlflow/sklearn/wine/data/wine-quality.csv"
 
+  # configure Docker: https://cloud.google.com/container-registry/docs/advanced-authentication#gcloud-helper
+  gcloud auth configure-docker
+
   install_packages "moreutils"
 
   # update specification files (docker image tags)
