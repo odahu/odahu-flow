e6d3e1fcc2cdce50cb6268f4e9c25b380634fcc1
BPylypenko <49116723+BPylypenko@users.noreply.github.com>
2021-01-26 16:55:43 +0200

    fix. selecting "base64" cmd flags depending on UNIX-like OS in use

--- packages/tests/stuff/training_stuff.sh
+++ packages/tests/stuff/training_stuff.sh
@@ -244,11 +244,15 @@ function local_setup() {
 
   install_packages "moreutils"
 
+  CMDbase64=(base64 -w0)
+  if  [ -x "$(command -v brew)" ];  then CMDbase64=base64
+  fi
+
   # update specifications
   ## docker-pull target
   local docker_uri="$(jq -r .docker_repo "${CLUSTER_PROFILE}")"
   local docker_username="$(jq -r .docker_username "${CLUSTER_PROFILE}")"
-  local docker_password="$(jq -r .docker_password "${CLUSTER_PROFILE}" | tr -d "\n" | base64)"
+  local docker_password="$(jq -r .docker_password "${CLUSTER_PROFILE}" | tr -d "\n" | $CMDbase64)"
 
   jq --arg uri "${docker_uri}" --arg username "${docker_username}" --arg password "${docker_password}" \
   '.spec.uri=$uri | .spec.username=$username | .spec.password=$password' "${LOCAL_TEST_DATA}/odahuflow/dir/docker-pull-target.json" | jq "." | sponge "${LOCAL_TEST_DATA}/odahuflow/dir/docker-pull-target.json"
