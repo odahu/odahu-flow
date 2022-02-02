6afb714986fa20b8ab294f96aa9f814009405471
BPylypenko <49116723+BPylypenko@users.noreply.github.com>
2022-02-02 12:34:19 +0200

    debug

--- packages/tests/stuff/training_stuff.sh
+++ packages/tests/stuff/training_stuff.sh
@@ -220,6 +220,7 @@ function cleanup() {
   odahuflowctl conn delete --id ${TEST_VALID_GPPI_ODAHU_FILE_ID} --ignore-not-found
   odahuflowctl conn delete --id ${TEST_CUSTOM_OUTPUT_FOLDER} --ignore-not-found
 
+  echo NO_CLEAN_UP=${NO_CLEAN_UP}
   if [ "${NO_CLEAN_UP}" = "false" ]; then
     odahuflowctl conn delete --id ${TEST_WINE_CONN_ID} --ignore-not-found
   fi
